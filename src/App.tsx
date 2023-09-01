32e69ae3ece6adb0ae7a5a97332422f57548ec87
Sandali <sandali.s@hsenidmobile.com>
2023-09-01 09:29:49 +0530

    setting up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import "./index.css";
+
+const API_KEY = "8fc4e46e822580d5655a161dd7b302d5";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
