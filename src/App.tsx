201b2f3f48499d0c93b8e11fcd16ca4a5c243d45
Sandali <sandali.s@hsenidmobile.com>
2023-09-05 10:05:59 +0530

    complete functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const api = {
-  key: "8fc4e46e822580d5655a161dd7b302d5",
-  base: "https://api.openweathermap.org/data/2.5/weather",
+  key: "e211f1ffa8e54e409f065d7a2e3dbb92",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
-function App() {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
+interface WeatherData {
+  name: string;
+  sys: {
+    country: string;
+  };
+  main: {
+    temp: number;
+  };
+  weather: {
+    description: string;
+  }[];
+}
 
-  const search = (evt: React.KeyboardEvent<HTMLInputElement>) => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPIO=${api.key}`)
-        .then((res) => res.json())
-        .then((result) => setWeather(result));
+function App() {
+  const [query, setQuery] = useState<string>("");
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`
+      );
+
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+
+      const data: WeatherData = await response.json();
+      setWeather(data);
+      setQuery("");
+      setErrorMessage(null); // Clear any previous error messages
+    } catch (error) {
+      console.error(error);
+      setWeather(null);
+      setErrorMessage("City not found"); // Set the error message
     }
   };
 
-  const today = new Date();
   const dateBuilder = (d: Date) => {
-    let months = [
+    const months = [
       "January",
       "February",
       "March",
@@ -34,7 +60,7 @@ function App() {
       "November",
       "December",
     ];
-    let days = [
+    const days = [
       "Sunday",
       "Monday",
       "Tuesday",
@@ -43,28 +69,59 @@ function App() {
       "Friday",
       "Saturday",
     ];
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
 
     return `${day} ${date} ${month} ${year}`;
+  };
+
+  const handleButtonClick = () => {
+    if (query.trim() !== "") {
+      fetchData();
+    } else {
+      setErrorMessage("Please enter a city");
+    }
   };
 
   return (
     <div className="App">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..." />
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                handleButtonClick(); // Fetch data when Enter key is pressed
+              }
+            }}
+          />
+          <button className="search-button" onClick={handleButtonClick}>
+            Enter
+          </button>
         </div>
-        <div className="location-box">
-          <div className="location">New York,US</div>
-          <div className="date">{dateBuilder(today)}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15 C</div>
-          <div className="weather">Sunny</div>
-        </div>
+
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+
+        {weather && weather.main && (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].description}</div>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
